57116d356c75241e390ccc8c54050ee71517d21e
Miah Johnson <miah@cx.com>
2012-03-21 20:57:30 -0700

    Lets also ensure that the datadir actually exists.

--- recipes/_server_config.rb
+++ recipes/_server_config.rb
@@ -8,6 +8,12 @@ directory node['redis']['conf_dir'] do
   mode 0755
 end
 
+directory node['redis']['config']['dir'] do
+  owner node['redis']['user']
+  group node['redis']['group']
+  mode 0755
+end
+
 template "#{node['redis']['conf_dir']}/redis.conf" do
   source "redis.conf.erb"
   owner "root"
