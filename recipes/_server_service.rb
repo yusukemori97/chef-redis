0269066c16b730dab3a9c6260164a0ccf4f16e37
Sean Porter <portertech@gmail.com>
2012-04-20 16:11:52 -0700

    [service] service resource service_name is based on platform

--- recipes/_server_service.rb
+++ recipes/_server_service.rb
@@ -3,6 +3,6 @@
 # Recipe:: _server_service
 
 service "redis" do
-  service_name "redis-server"
+  service_name node['platform'] =~ /ubuntu|debian/ ? "redis-server" : "redis"
   action [ :enable, :start ]
 end
