00927bb83ec5e2a6b99fb825fb20b6642b6403a2
Sean Porter <portertech@gmail.com>
2012-04-20 16:26:31 -0700

    [service] use a case to make joemiller happy, future in mind

--- recipes/_server_service.rb
+++ recipes/_server_service.rb
@@ -2,7 +2,16 @@
 # Cookbook Name:: redis
 # Recipe:: _server_service
 
+redis_service = case node['platform']
+when "ubuntu", "debian"
+  "redis-server"
+when "centos", "redhat"
+  "redis"
+else
+  "redis"
+end
+
 service "redis" do
-  service_name node['platform'] =~ /ubuntu|debian/ ? "redis-server" : "redis"
+  service_name redis_service
   action [ :enable, :start ]
 end
