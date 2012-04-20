3620303ea096c9ede823736a03b3a9c53e3014b0
Anthony Goddard <anthony@anthonygoddard.com>
2012-04-20 15:19:24 -0400

    version bump, fixed copyright and recipe name

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,8 +1,8 @@
 #
 # Cookbook Name:: redis
-# Recipe:: default
+# Recipe:: server
 #
-# Copyright 2010, Atari, Inc
+# Copyright 2012 Woods Hole Marine Biological Laboratory
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
