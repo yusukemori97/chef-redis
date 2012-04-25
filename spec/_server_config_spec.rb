513050a4359c990c956d3f86406c9f7ef86455d0
Miah Johnson <miah@cx.com>
2012-04-25 16:31:37 -0700

    herp derp syntax.

--- spec/_server_config_spec.rb
+++ spec/_server_config_spec.rb
@@ -2,12 +2,25 @@ require "chefspec"
 
 describe "redis::_server_init" do
   let (:chef_run) {ChefSpec::ChefRunner.new.converge 'redis::_server_init'}
-  it "should create /etc/init.d/redis-server" do
-    chef_run.should create_file '/etc/init.d/redis-server'
+  it "should create directory /etc/redis" do
+    chef_run.node.redis.conf_dir = '/etc/redis'
+    chef_run.should create_directory '/etc/redis'
   end
-  it "should set owner 'root' on /etc/init.d/redis-server"
-    chef_run.file('/etc/init.d/redis-server').should be_owned_by('root', 'root')
+  it "should create directory /var/lib/redis" do
+    chef_run.node.redis.config.dir = '/var/lib/redis'
+    chef_run.should create_directory '/var/lib/redis'
   end
-  it "should set permission mode 0755 on /etc/init.d/redis-server"
+  it "should set owner 'root' and group 'root' on /etc/redis" do
+    chef_run.file('/etc/redis').should be_owned_by('root', 'root')
+  end
+  it "should set owner 'root' and group 'root' on /var/lib/redis" do
+    chef_run.file('/var/lib/redis').should be_owned_by('root', 'root')
+  end
+  it "should create /etc/redis/redis.conf" do
+    chef_run.should create_file '/etc/redis/redis.conf'
+  end
+  it "should set permission mode 0755 on /etc/redis" do
+  end
+  it "should set permission mode 0755 on /var/lib/redis" do
   end
 end
--- spec/_server_init_spec.rb
+++ spec/_server_init_spec.rb
@@ -5,9 +5,9 @@ describe "redis::_server_init" do
   it "should create /etc/init.d/redis-server" do
     chef_run.should create_file '/etc/init.d/redis-server'
   end
-  it "should set owner 'root' on /etc/init.d/redis-server"
+  it "should set owner 'root' on /etc/init.d/redis-server" do
     chef_run.file('/etc/init.d/redis-server').should be_owned_by('root', 'root')
   end
-  it "should set permission mode 0755 on /etc/init.d/redis-server"
+  it "should set permission mode 0755 on /etc/init.d/redis-server" do
   end
 end
