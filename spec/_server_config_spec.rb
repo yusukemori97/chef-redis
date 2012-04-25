0b81d2982bf2f7349a76ef23510380ffbb9ea2b0
Miah Johnson <miah@cx.com>
2012-04-25 16:23:08 -0700

    Add rough specs for _server_config, and _server_init.

---
+++ spec/_server_config_spec.rb
@@ -0,0 +1,13 @@
+require "chefspec"
+
+describe "redis::_server_init" do
+  let (:chef_run) {ChefSpec::ChefRunner.new.converge 'redis::_server_init'}
+  it "should create /etc/init.d/redis-server" do
+    chef_run.should create_file '/etc/init.d/redis-server'
+  end
+  it "should set owner 'root' on /etc/init.d/redis-server"
+    chef_run.file('/etc/init.d/redis-server').should be_owned_by('root', 'root')
+  end
+  it "should set permission mode 0755 on /etc/init.d/redis-server"
+  end
+end
