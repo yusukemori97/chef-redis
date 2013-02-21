9363c83ed2b29084870e2f010f9e708a2e7f8aa5
Chris Lundquist <chris.lundquist@bluebox.net>
2013-02-20 16:21:49 -0800

    [replication] initial import

---
+++ attributes/sentinel.rb
@@ -0,0 +1,8 @@
+default.redis.sentinel.port             = 26379
+default.redis.sentinel.quorum           = 2
+default.redis.sentinel.down_after       = 60000
+default.redis.sentinel.failover_timeout = 900000
+default.redis.sentinel.can_failover     = true
+default.redis.sentinel.parallel_syncs   = 1
+default.redis.sentinel.config.daemonize = true
+default.redis.sentinel.config.pidfile   = "/var/run/redis/redis-sentinel.pid"
