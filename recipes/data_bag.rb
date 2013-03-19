f12389e23b3a9537a91c5b9280d8d05d140b2c45
Sam Cooper <sam@chgworks.com>
2013-03-19 00:19:23 -0700

    [slaveof] data_bag recipe will recieve all input from databag (not node attrs)

--- recipes/data_bag.rb
+++ recipes/data_bag.rb
@@ -4,21 +4,11 @@ bag = node.redis.data_bag_name
 
 node.redis.instances.each do |instance|
   instance_data = data_bag_item( bag, instance )
-
-  redis_instance instance do
-
-    if node.redis.config.slaveof_ip
-      send("slaveof_ip", node.redis.config.slaveof_ip)
-    end
+  name = instance_data["name"] || instance
     
+  redis_instance name do
     instance_data.each do |attribute,value|
-      
-      if attribute == "port"
-        send("slaveof_port", value)
-      end
-      
       send(attribute, value) unless attribute == "id"
     end
-    
   end
 end
