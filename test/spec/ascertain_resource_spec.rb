60054f06d678f7a02e1a0968b952bd59f16d77cb
Miah Johnson <miah@chia-pet.org>
2013-11-28 00:24:37 -0800

    Stub spec.

---
+++ test/spec/ascertain_resource_spec.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+
+describe AscertainResource do
+  describe 'current redis configuration' do
+
+    it 'should return a hash' do
+      true.should == false
+    end
+
+  end
+end
